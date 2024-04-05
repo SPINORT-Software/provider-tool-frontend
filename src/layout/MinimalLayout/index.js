2e80a9df116b9ec8b48f09e13f8f2f407194fb7a
Sharan Chitta <chitta.sharan@gmail.com>
2024-04-05 18:07:14 -0400

    Clear

--- src/layout/MinimalLayout/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-
-// ===========================|| MINIMAL LAYOUT ||=========================== //
-
-const MinimalLayout = () => <Outlet />;
-
-export default MinimalLayout;
